988a23d672c4e52a8e745487ad4aa7d03e2d90ce
yunseochoi(home) <jamie0730@naver.com>
2023-10-04 20:47:30 +0900

    작은 공이 아래로 떨어지다가 땅에 닿으면 멈추는 기능 구현 (ball frame 수정 및 땅에 닿지 않는 오류 수정)

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -35,11 +35,25 @@ class Big_Ball:
 
     def update(self):
         self.frame = random.randint(0, 0)
-        if self.y>90:
+        if self.y>70:
             self.y -= 5
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 41, 0, 41, 41, self.x, self.y)
+
+class Small_Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.frame = 0
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        self.frame = random.randint(0, 0)
+        if self.y>60:
+            self.y -= 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 21, 0, 21, 21, self.x, self.y)
 
 def handle_events():
     global running
@@ -56,6 +70,7 @@ def reset_world():
     global grass
     global team
     global big_balls
+    global small_balls
     global world
 
     running = True
@@ -66,8 +81,10 @@ def reset_world():
 
     team = [Boy() for i in range(11)]
     big_balls = [Big_Ball() for i in range(10)]
+    small_balls = [Small_Ball() for i in range(10)]
     world += team
     world += big_balls
+    world += small_balls
 
 def update_world():
     grass.update()
@@ -75,6 +92,8 @@ def update_world():
         boy.update()
     for big_ball in world:
         big_ball.update()
+    for small_ball in world:
+        small_ball.update()
 
 def render_world():
     clear_canvas()
@@ -82,6 +101,8 @@ def render_world():
         boy.draw()
     for big_ball in world:
         big_ball.draw()
+    for small_ball in world:
+        small_ball.draw()
     update_canvas()
 
 open_canvas()
